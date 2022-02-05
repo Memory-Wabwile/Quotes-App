9338e00422de43da6f0e3619635e987bd15a7d4c
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-05 15:29:26 +0300

    add logic of calculating days to the pipe

--- src/app/day-count.pipe.ts
+++ src/app/day-count.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dayCount'
+  name: 'dayCount',
 })
 export class DayCountPipe implements PipeTransform {
+  transform(value: any): number {
+    let today: Date = new Date();
+    let todaysTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs( todaysTime -value ) 
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (dateCounter >= 1 && value > todaysTime){
+      return dateCounter;
+    }else{
+    return 0;
   }
-
+}
 }
