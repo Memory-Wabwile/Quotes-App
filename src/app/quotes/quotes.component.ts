a25e8b418b50a3b93f57e71cf394cc3b71b2ce3d
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-05 21:16:41 +0300

    link the form to quote component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,34 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import{Quotes} from '../quotes';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  styleUrls: ['./quotes.component.css'],
 })
 export class QuotesComponent implements OnInit {
-
-  quotes:Quotes[]=[
-    new Quotes (1,'Life is what happens when you are busy making other plans.' , 'John Lennon' ,'John Lennon', new Date(2020,8,10)),
-    new Quotes(2,'The greatest glory in living lies not in never falling, but in rising every time we fall' , 'Nelson Mandela' ,'Nelson Mandela',new Date(2021,12,12)),
-    new Quotes(3,'Live as if you were to die tomorrow. Learn as if you were to live forever.' , 'Mahatma Gandhi' ,'Mahatma Gandhi',new Date(2022,1,1)),
-    new Quotes (4,'Be who you are and say what you feel, because those who mind don not matter and those who matter do not mind.' , 'Bernard M. Baruch' ,'Bernard M. Baruch',new Date(2021,6,5)),
-    new Quotes(5,'If life were predictable it would cease to be life, and be without flavor.' , 'Eleanor Roosevelt' ,'Eleanor Roosevelt' , new Date(2019,2,1)),
-    
+  quotes: Quotes[] = [
+    new Quotes(
+      1,
+      'Life is what happens when you are busy making other plans.',
+      'John Lennon',
+      'John Lennon',
+      new Date(2020, 8, 10)
+    ),
+    new Quotes(
+      2,
+      'The greatest glory in living lies not in never falling, but in rising every time we fall',
+      'Nelson Mandela',
+      'Nelson Mandela',
+      new Date(2021, 12, 12)
+    ),
+    new Quotes(
+      3,
+      'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+      'Mahatma Gandhi',
+      'Mahatma Gandhi',
+      new Date(2022, 1, 1)
+    ),
+    new Quotes(
+      4,
+      'Be who you are and say what you feel, because those who mind don not matter and those who matter do not mind.',
+      'Bernard M. Baruch',
+      'Bernard M. Baruch',
+      new Date(2021, 6, 5)
+    ),
+    new Quotes(
+      5,
+      'If life were predictable it would cease to be life, and be without flavor.',
+      'Eleanor Roosevelt',
+      'Eleanor Roosevelt',
+      new Date(2019, 2, 1)
+    ),
   ];
 
-  deleteQuote(isDone:any,index:any){
-    if (isDone){
-      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].author}'s quote?`);
-     
+  addNewQuote(quote: any) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
+  }
+
+  deleteQuote(isDone: any, index:any) {
+    if (isDone) {
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].author}'s quote?`
+      );
+
       if (toDelete) {
         this.quotes.splice(index, 1);
-      } 
+      }
     }
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() :void {}
-
+  ngOnInit(): void {}
 }
