05c58f968bd83113d7c97c617f5b054ce8f3ba66
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-05 17:29:22 +0300

    generate the quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { QuotesComponent } from './quotes/quotes.component';
 
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DayCountPipe } from './day-count.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
-  declarations: [AppComponent, QuotesComponent, QuoteDetailsComponent, DayCountPipe],
+  declarations: [AppComponent, QuotesComponent, QuoteDetailsComponent, DayCountPipe, QuotesFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
