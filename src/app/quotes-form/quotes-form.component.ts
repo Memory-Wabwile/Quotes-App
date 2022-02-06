5a004bc19de240fc477916692b7e46101b1e686b
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-06 10:47:12 +0300

    link modal form to add quote button

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit , Output, EventEmitter } from '@angular/core';
-import{Quotes} from '../quotes';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import { format } from 'path';
+import { Quotes } from '../quotes';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
-  styleUrls: ['./quotes-form.component.css']
+  styleUrls: ['./quotes-form.component.css'],
 })
 export class QuotesFormComponent implements OnInit {
-
-
-  newQuote = new Quotes(0,"","","",new Date());
+  newQuote = new Quotes(0, '', '', '', new Date());
   @Output() addQuote = new EventEmitter<Quotes>();
-  
+
   // addNewQuote:Quotes | undefined;
 
-  submitQuote(){
+  submitQuote() {
     this.addQuote.emit(this.newQuote);
-      }
-  constructor() { }
-
-  ngOnInit(): void {
+    // form.resetForm();
   }
 
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
